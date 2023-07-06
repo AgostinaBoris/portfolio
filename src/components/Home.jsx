a527cbd56182fcdaced3c7e877436789a4435bd0
Agostina Boris <agostinaboris@protonmail.com>
2023-07-06 13:47:51 -0300

    se agregan archivos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 export default function Home() {
   return (
@@ -14,13 +15,17 @@ export default function Home() {
           I'm a Jr. Front-end Web Developer.
         </h2>
         <p className="text-[#8892b0] text-xl py-4 max-w-[700px]">
-          I'm a junior front-end developer with a passion for creating engaging digital experiences. 
-          I'm focused on building responsive web applications and continuously expanding my skills in this area. 
+          I'm a junior front-end developer with a passion for creating engaging
+          digital experiences. I'm focused on building responsive web
+          applications and continuously expanding my skills in this area.
         </p>
         <div>
-          <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-amber-400 hover:border-amber-400 ">
-            View Work <HiArrowNarrowRight />
-          </button>
+          <Link
+            to="work"
+            className="text-white button-style border-2 px-6 py-3 my-2 flex items-center hover:bg-amber-400 w-1/6 hover:border-amber-400"
+          >
+            View Work <HiArrowNarrowRight />{" "}
+          </Link>
         </div>
       </div>
     </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import Tesla from '../assets/tesla.jpg';
-import Ecommerce from '../assets/ecommerce.jpg';
-import Nikko from '../assets/nikko.jpg';
-import Clima from '../assets/clima.jpg';
-import Homepage from '../assets/homePage.jpg';
+import Tesla from "../assets/tesla.jpg";
+import Ecommerce2 from "../assets/ecommerce.jpg";
+import Nikko from "../assets/nikko.jpg";
+import Clima from "../assets/clima.jpg";
+import Homepage from "../assets/homePage.jpg";
+import pic from "../assets/sugar.jpg";
+import Sushi from "../assets/sushi.jpg";
 
 const Work = () => {
   return (
@@ -19,124 +21,172 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
-             style={{ backgroundImage: `url(${Tesla})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div">
-            {/* Hover effects */}
+            style={{ backgroundImage: `url(${pic})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
+       
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Tesla Landing Page - Tailwind Css 
+                Ecommerce NextJS-Tailwind
               </span>
-                <div>
-                  <a href="https://tailwind-dz85asx9r-agostinaboris.vercel.app/">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Demo
-                    </button>
-                  </a>
-                  <a href="https://github.com/AgostinaBoris/Landing_page.git">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Code
-                    </button>
-                  </a>
-                </div>
+              <div>
+                <a href="https://ecommerce-next-js-tailwind.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Ecommerce_NextJs_Tailwind.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
 
-
           <div
-             style={{ backgroundImage: `url(${Ecommerce})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div">
+            style={{ backgroundImage: `url(${Sushi})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
             {/* Hover effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Ecommerce No Functional - Html Css Javascript
+               Sushi Website - React Js
               </span>
-                <div>
-                  <a href="https://ecommerce-html-css-js.vercel.app/index.html">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Demo
-                    </button>
-                  </a>
-                  <a href="https://github.com/AgostinaBoris/Ecommerce_html-Css-Js.git">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Code
-                    </button>
-                  </a>
-                </div>
+              <div>
+                <a href="https://sushi-web-eight.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Sushi_web">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
 
-
           <div
-             style={{ backgroundImage: `url(${Nikko})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div">
+            style={{ backgroundImage: `url(${Nikko})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
             {/* Hover effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 NextJs Website
               </span>
-                <div>
-                  <a href="https://next-js-web-one.vercel.app">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Demo
-                    </button>
-                  </a>
-                  <a href="https://github.com/AgostinaBoris/NextJs_Web.git">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Code
-                    </button>
-                  </a>
-                </div>
-            </div>
-          </div>
-          
-          <div
-             style={{ backgroundImage: `url(${Clima})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto  content-div">
-            {/* Hover effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Weather App - This one maybe is not working, i had to change the api key.
-              </span>
-                <div>
-                  <a href="https://weather-app-delta-sable.vercel.app/">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Demo
-                    </button>
-                  </a>
-                  <a href="https://github.com/AgostinaBoris/Weather_app.git">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Code
-                    </button>
-                  </a>
-                </div>
+              <div>
+                <a href="https://next-js-web-one.vercel.app">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/NextJs_Web.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
 
           <div
-             style={{ backgroundImage: `url(${Homepage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div">
+            style={{ backgroundImage: `url(${Tesla})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Tesla Landing Page - Tailwind Css
+              </span>
+              <div>
+                <a href="https://tailwind-dz85asx9r-agostinaboris.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Landing_page.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Ecommerce2})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Ecommerce Model - Html Css Javascript
+              </span>
+              <div>
+                <a href="https://ecommerce-html-css-js.vercel.app/index.html">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Ecommerce_html-Css-Js.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Homepage})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto content-div"
+          >
             {/* Hover effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 Homepage - Html Css Js
               </span>
-                <div>
-                  <a href="https://homepage-main-iota.vercel.app/">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Demo
-                    </button>
-                  </a>
-                  <a href="https://github.com/AgostinaBoris/Homepage_main.git">
-                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                        Code
-                    </button>
-                  </a>
-                </div>
+              <div>
+                <a href="https://homepage-main-iota.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Homepage_main.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
 
-          
-
+          <div
+            style={{ backgroundImage: `url(${Clima})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center m-auto  content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Weather App 
+              </span>
+              <div>
+                <a href="https://weather-app-delta-sable.vercel.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/AgostinaBoris/Weather_app.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
